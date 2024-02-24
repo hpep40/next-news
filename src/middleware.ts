61044fa7b7de709860df8770fc3d018f408712c6
elisawitt <elisaweibert@outlook.com>
2024-02-24 21:34:52 +0700

    adds force redirect to www only

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,20 @@
+import { NextRequest, NextResponse } from "next/server"
 import createMiddleware from "next-intl/middleware"
 import { i18n } from "./i18n/i18n"
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   locales: i18n.locales,
   defaultLocale: i18n.defaultLocale,
   localePrefix: "always",
 })
+
+export default function middleware(request: NextRequest) {
+  if (request.nextUrl.hostname === "bobzar.com") {
+    return NextResponse.redirect(`https://www.${request.nextUrl.hostname}${request.nextUrl.pathname}`)
+  }
+
+  return intlMiddleware
+}
 
 export const config = {
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
